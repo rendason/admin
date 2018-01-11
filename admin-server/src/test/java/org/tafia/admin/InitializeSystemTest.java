34fadaa62d4bc8b105a6e6e64bdf303b77ec4690
Dason <rendason@outlook.com>
2018-01-11 22:22:00 +0800

    add test case

--- admin-server/src/test/java/org/tafia/admin/InitializeSystemTest.java
+++ admin-server/src/test/java/org/tafia/admin/InitializeSystemTest.java
@@ -1,18 +1,24 @@
 package org.tafia.admin;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.tafia.admin.modules.employee.dao.UserDao;
 import org.tafia.admin.modules.employee.model.User;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("dev")
+@WithMockUser(username = "admin", authorities = "owner")
+@Ignore
 public class InitializeSystemTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
@@ -26,7 +32,14 @@ public class InitializeSystemTest extends AbstractTestNGSpringContextTests {
         User user = new User();
         user.setUsername("admin");
         user.setPassword(passwordEncoder.encode("tafia.org"));
-        user.setRoles(Collections.singleton("admin"));
+        user.setRoles(Collections.singleton("owner"));
+        user.setSalary(new BigDecimal("23222.20"));
         userDao.save(user);
+    }
+
+    @Test
+    public void testFindByName() {
+        User user = userDao.findByUsername("admin");
+        System.out.println(JSON.toJSONString(user));
     }
 }
